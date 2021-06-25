082b9194073c20c9fb495abab75938c1f39c7826
sckemu <you@example.com>
2021-06-25 23:21:14 +0900

    modified:   .config/fish/config.fish 	typechange: .tmux.conf

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,9 @@
 
 . "$HOME/.homesick/repos/homeshick/homeshick.fish"
 source "$HOME/.homesick/repos/homeshick/homeshick.fish"
+
+set fish_plugins theme peco
+
+function fish_user_key_bindings
+  bind \cr peco_select_history # Bind for prco history to Ctrl+r
+end
