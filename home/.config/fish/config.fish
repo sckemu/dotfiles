03a59f6f0437ab0468c11ea8dc0c4f77b502ca45
sckemu <you@example.com>
2021-06-25 23:32:41 +0900

    modified:   home/.config/fish/config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,3 +7,5 @@ set fish_plugins theme peco
 function fish_user_key_bindings
   bind \cr peco_select_history # Bind for prco history to Ctrl+r
 end
+
+. "$HOME/.homesick/repos/homeshick/homeshick.fish"
